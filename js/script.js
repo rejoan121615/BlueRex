2fc3fd88f1a15b547d4d811661ab4df079e7d79a
Mohd Rejoan <wearecrx@gmail.com>
2021-04-02 16:21:33 +0600

    fix menu color, make slider offline, fix navbar menu, fix section color

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ let condition = 1;
 
 function changeImg() {
     if (condition) {
-        resNavBtn.style.backgroundImage = 'url(\'../img/close.png\')';
+        resNavBtn.style.backgroundImage = 'url(\'./img/close.png\')';
         NavBar.classList.add('active');
         condition--;
     } else {
-        resNavBtn.style.backgroundImage = "url('\../img/menu.png\')";
+        resNavBtn.style.backgroundImage = "url('\./img/menu.png\')";
         NavBar.classList.remove('active');
         condition++;
     }
